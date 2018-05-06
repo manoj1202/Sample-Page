7596ac89349c1c18a4edb5a9f1e9f8c83f2006e5
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-06 15:09:45 -0500

    Sample

--- todo.js
+++ todo.js
@@ -4,12 +4,14 @@
 
 document.querySelector('#form1').addEventListener('submit', function(e){
     e.preventDefault();
-    console.log(e.target.elements.todoValue.value);
+    
     const p = document.createElement('p');
-    const button = document.createElement('button');
     p.textContent = e.target.elements.todoValue.value ;
-    button.innerHTML = "x";
     document.querySelector('#head1').appendChild(p);
-    document.querySelector('#head1').appendChild(button);
+    
     e.target.elements.todoValue.value = '';
+   
 })
+let a = []; 
+a.push(e.target.elements.todoValue.value);
+console.log(a);
