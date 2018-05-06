77730a1aef689262d4b8b51acd9c98c585b62944
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-06 16:34:44 -0500

    Sample

--- todo.js
+++ todo.js
@@ -2,16 +2,45 @@
 //     console.log(e.target.value)
 // })
 
-document.querySelector('#form1').addEventListener('submit', function(e){
-    e.preventDefault();
+// document.querySelector('#form1').addEventListener('submit', function(e){
+//     e.preventDefault();
+//     let a = [];
+//     const p = document.createElement('p');
+//     p.textContent = e.target.elements.todoValue.value ;
+//     document.querySelector('#head1').appendChild(p);
     
-    const p = document.createElement('p');
-    p.textContent = e.target.elements.todoValue.value ;
-    document.querySelector('#head1').appendChild(p);
+//     a.push(e.target.elements.todoValue.value);
     
-    e.target.elements.todoValue.value = '';
+    
+//     console.log(a);
+//     e.target.elements.todoValue.value = '';
+// })
+
+let toDoList = ['Hit the gym','Pay Bill','Meet George'];
+
+const renderList = () =>{
+
+   let myTodo =  document.getElementById('TODO');
+    
+   for(const todo in toDoList){
    
-})
-let a = []; 
-a.push(e.target.elements.todoValue.value);
-console.log(a);
+    const p  =   document.createElement('p');
+
+       p.textContent = toDoList[todo];
+        
+       myTodo.appendChild(p);
+
+    }
+}
+
+const addTodo = () => {
+    const addTodo = document.getElementById('data').value;
+    if(addTodo !=='' && addTodo !== ' ' )
+    {
+        toDoList.push(addTodo);
+        
+    }
+}
+
+renderList();
+addTodo();
