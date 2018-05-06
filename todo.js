34066c6d86191ebc09c5889c6b76c043bbb3af9b
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-06 12:37:09 -0500

    creating page

--- todo.js
+++ todo.js
@@ -1,4 +1,9 @@
-let input1 = document.querySelector('button').addEventListener('click',function(){
+document.querySelector('button').addEventListener('click',function(e){
     console.log('did this work');
+    console.log(e);
 })
 
+document.querySelector('#userinput').addEventListener('input',function(e){
+    console.log(e.target.vlaue);
+})
+//const para = document.querySelector('p').
