dd325a984b3c4573677f387a6b1975bdf7c517f8
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-06 13:26:17 -0500

    Sample

--- todo.js
+++ todo.js
@@ -1,3 +1,12 @@
-document.querySelector('#input').addEventListener('input',function(e){
-    console.log(e.target.value)
+// document.querySelector('#input').addEventListener('input',function(e){
+//     console.log(e.target.value)
+// })
+
+document.querySelector('#form1').addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log(e.target.elements.todoValue.value);
+    const p = document.createElement('p');
+    p.textContent = e.target.elements.todoValue.value ;
+    document.querySelector('#head1').appendChild(p);
+    e.target.elements.todoValue.value = '';
 })
